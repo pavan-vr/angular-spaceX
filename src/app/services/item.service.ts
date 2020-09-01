d71b377c96e6e151c7e1875501bb787dccd1ef8e
pavan v r <pavan.vr@hashedin.com>
2020-09-01 17:24:40 +0530

    deploying

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IrocketCard } from '../rocketCard';
+const BASE_URL = 'https://api.spaceXdata.com/v3/launches?limit=100';
 @Injectable({
   providedIn: 'root',
 })
 export class ItemService {
-  public url = 'https:api.spaceXdata.com/v3/launches?limit=100';
   private filterURL;
 
   constructor(private http: HttpClient) {}
 
   getItems(): Observable<IrocketCard[]> {
-    return this.http.get<IrocketCard[]>(this.url);
+    return this.http.get<IrocketCard[]>(BASE_URL);
   }
 
   getFilterItems(year, launchStatus, landStatus): Observable<IrocketCard[]> {
-    this.filterURL = this.url;
+    this.filterURL = BASE_URL;
     if (year !== ' ') {
       this.filterURL = this.filterURL + '&launch_year=' + year;
     }
