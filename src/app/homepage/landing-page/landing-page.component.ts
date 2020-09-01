3f8eb40802e60baf29f280409f0906b53c76996c
pavan v r <pavan.vr@hashedin.com>
2020-09-01 16:53:20 +0530

    adding some responsiveness

--- src/app/homepage/landing-page/landing-page.component.ts
+++ src/app/homepage/landing-page/landing-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../services/item.service';
 import { IrocketCard } from '../../rocketCard';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
-  constructor(private itemService: ItemService) {}
+  constructor(private itemService: ItemService, private router: Router) {}
   public rocketData: IrocketCard[];
   public year: string;
   public launchStatus: boolean;
